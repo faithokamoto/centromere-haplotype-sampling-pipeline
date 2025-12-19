ca4607cc3a5200c16af932b7f2cd0b8493c01a85
faithokamoto <faith.purpleflower@gmail.com>
2025-12-19 14:16:25 -0800

    mark done

--- run_test_samples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Run the 21 test samples through the pipeline
-# Usage: run_test_samples.sh
-
-while IFS= read -r line; do
-    haplotype=$(echo "$line" | cut -f3 -d ",")
-    ./guess_optimal_num_sampled_haplo.py "$haplotype"
-    echo "============="
-done < test_samples.txt
