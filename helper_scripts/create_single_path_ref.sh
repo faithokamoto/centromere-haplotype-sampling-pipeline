1d8d79ba54d5af9f01c58a050a1876170d7ebc5a
faithokamoto <faith.purpleflower@gmail.com>
2026-03-27 09:27:32 -0700

    always bother

--- helper_scripts/create_single_path_ref.sh
+++ helper_scripts/create_single_path_ref.sh
@@ -10,19 +10,17 @@ OUT=$3
 SAMPLE_ID=`echo "$PATH_NAME" | cut -f1 -d "#"`
 
 # Only bother if needed
-if [ ! -f ${OUT}.mmi ]; then
-    vg mod --keep-path "$PATH_NAME" "$BIG_GRAPH" > ${OUT}.vg
-    # Convert to GBZ
-    vg gbwt --index-paths -x ${OUT}.vg -o ${OUT}.no_ref.gbwt
-    vg gbwt -o ${OUT}.with_ref.gbwt --set-reference "$SAMPLE_ID" ${OUT}.no_ref.gbwt
-    vg gbwt --gbz-format -x ${OUT}.vg ${OUT}.with_ref.gbwt -g ${OUT}.gbz
-    # Now that we have the GBZ we don't need these other things
-    rm ${OUT}.no_ref.gbwt ${OUT}.with_ref.gbwt ${OUT}.vg
-    # Index
-    vg autoindex --gbz ${OUT}.gbz -w lr-giraffe --prefix "$OUT" --no-guessing
-    # Minimap2 needs FASTA input
-    vg paths --extract-fasta -x ${OUT}.gbz > ${OUT}.fasta
-    # Avoid reusing an old index
-    rm -f "${OUT}.fasta.fai"
-    minimap2 -x map-hifi -d ${OUT}.mmi ${OUT}.fasta
-fi
+vg mod --keep-path "$PATH_NAME" "$BIG_GRAPH" > ${OUT}.vg
+# Convert to GBZ
+vg gbwt --index-paths -x ${OUT}.vg -o ${OUT}.no_ref.gbwt
+vg gbwt -o ${OUT}.with_ref.gbwt --set-reference "$SAMPLE_ID" ${OUT}.no_ref.gbwt
+vg gbwt --gbz-format -x ${OUT}.vg ${OUT}.with_ref.gbwt -g ${OUT}.gbz
+# Now that we have the GBZ we don't need these other things
+rm ${OUT}.no_ref.gbwt ${OUT}.with_ref.gbwt ${OUT}.vg
+# Index
+vg autoindex --gbz ${OUT}.gbz -w lr-giraffe --prefix "$OUT" --no-guessing
+# Minimap2 needs FASTA input
+vg paths --extract-fasta -x ${OUT}.gbz > ${OUT}.fasta
+# Avoid reusing an old index
+rm -f "${OUT}.fasta.fai"
+minimap2 -x map-hifi -d ${OUT}.mmi ${OUT}.fasta
