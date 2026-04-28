5be3c41fe90df97c1a38a50fec7282b1da1faf65
faithokamoto <faith.purpleflower@gmail.com>
2026-04-28 14:09:07 -0700

    it takes input

--- get_inputs/get_unsampled_gbz.sh
+++ get_inputs/get_unsampled_gbz.sh
@@ -3,6 +3,8 @@
 # Usage: get_unsampled_gbz.sh <chrom>
 # Example: get_unsampled_gbz.sh chr12
 
+CHROM=$1
+
 GFA_DIR=/private/groups/patenlab/mira/centrolign/batch_submissions/centrolign/release2_QC_v2/MSA
 GBZ_DIR=/private/groups/patenlab/fokamoto/centrolign/graph/unsampled
 
@@ -10,13 +12,13 @@ CHM13_NAME="CHM13#0#CHM13.0#0"
 CHM13_DIR=/private/groups/patenlab/fokamoto/centrolign/graph/haploid
 
 # Create combined GFA
-if [ -f $GFA_DIR/$chrom/${chrom}.centrolign.gfa ]; then
-    ./get_inputs/add_dummy_caps.py -o $GBZ_DIR/${chrom}.gfa $GFA_DIR/$chrom/${chrom}.centrolign.gfa
+if [ -f $GFA_DIR/$CHROM/${CHROM}.centrolign.gfa ]; then
+    ./get_inputs/add_dummy_caps.py -o $GBZ_DIR/${CHROM}.gfa $GFA_DIR/$CHROM/${CHROM}.centrolign.gfa
 else
-    ./get_inputs/add_dummy_caps.py -o $GBZ_DIR/${chrom}.gfa $GFA_DIR/$chrom/*/*.gfa
+    ./get_inputs/add_dummy_caps.py -o $GBZ_DIR/${CHROM}.gfa $GFA_DIR/$CHROM/*/*.gfa
 fi
 
 # Convert to GBZ
-./get_inputs/gfa_to_gbz_ref.sh $GBZ_DIR/$chrom
+./get_inputs/gfa_to_gbz_ref.sh $GBZ_DIR/$CHROM
 # Extract CHM13 ref
-./get_inputs/create_single_path_ref.sh $GBZ_DIR/${chrom}.pg "$CHM13_NAME" $CHM13_DIR/${chrom}.CHM13
+./get_inputs/create_single_path_ref.sh $GBZ_DIR/${CHROM}.pg "$CHM13_NAME" $CHM13_DIR/${CHROM}.CHM13
